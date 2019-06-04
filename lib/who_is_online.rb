689a037eee39af49bcffd1b047f1bd08d31b2ff1
andriy.kondzolko <kondzolko@gmail.com>
2019-06-03 20:42:53 -0400

    first commit

---
+++ lib/who_is_online.rb
@@ -0,0 +1,6 @@
+require "who_is_online/version"
+
+module WhoIsOnline
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/who_is_online/version.rb
@@ -0,0 +1,3 @@
+module WhoIsOnline
+  VERSION = "0.1.0"
+end
